abcd20cc764ebf30972a2806df79b5dd54672870
kagomen <fu.taz.1102@gmail.com>
2025-06-09 12:15:29 +0900

    springプロジェクトの作成

---
+++ src/main/java/com/example/bgm_api_springboot/BgmApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bgm_api_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgmApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgmApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bgm_api_springboot/BgmApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bgm_api_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgmApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
