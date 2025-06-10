40848f24904f110fa58fb403af202ac2578df976
kagomen <fu.taz.1102@gmail.com>
2025-06-10 15:07:01 +0900

    テーブル名の修正

--- src/main/java/com/example/bgm/entity/User.java
+++ src/main/java/com/example/bgm/entity/User.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "user")
+@Table(name = "`user`") // userはDBで予約語のため、バッククォートで囲まないとエラーが発生する
 @Getter
 @Setter
 @NoArgsConstructor
