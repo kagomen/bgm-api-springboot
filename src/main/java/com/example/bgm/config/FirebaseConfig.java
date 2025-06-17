ac27a912193e49d80b2cbc7b5b69db3958beb33b
kagomen <fu.taz.1102@gmail.com>
2025-06-17 14:51:29 +0900

    コメントの修正

--- src/main/java/com/example/bgm/config/FirebaseConfig.java
+++ src/main/java/com/example/bgm/config/FirebaseConfig.java
@@ -1,29 +1,26 @@
 package com.example.bgm.config;
 
-import java.io.IOException;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
 import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-/**
- * 初期化処理を定義
- */
+/** 初期化処理を定義 */
 @Configuration
 public class FirebaseConfig {
 
-  // @PostConstruct アプリ起動後一度だけ実行される
+  // アプリ起動後一度だけ実行される
   @PostConstruct
   public void initialize() throws IOException {
     if (FirebaseApp.getApps().isEmpty()) {
-      var serviceAccount = new ClassPathResource("firebase/serviceAccountKey.json").getInputStream();
+      var serviceAccount =
+          new ClassPathResource("firebase/serviceAccountKey.json").getInputStream();
 
-      var options = FirebaseOptions.builder()
+      var options =
+          FirebaseOptions.builder()
               .setCredentials(GoogleCredentials.fromStream(serviceAccount))
               .build();
 
