0604db61cd1c76ef013a33485acfe15627bbb15e
kagomen <fu.taz.1102@gmail.com>
2025-06-18 14:45:09 +0900

    クラス名の修正

--- src/main/java/com/example/bgm/service/UserService.java
+++ src/main/java/com/example/bgm/service/UserService.java
@@ -1,17 +1,19 @@
 package com.example.bgm.service;
 
 import com.example.bgm.entity.User;
-import com.example.bgm.repository.UserSRepository;
+import com.example.bgm.repository.UserRepository;
 import com.google.firebase.auth.FirebaseToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 // finalのついたフィールドのコンストラクターを自動生成
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
 
-  private final UserSRepository userRepository;
+  private final UserRepository userRepository;
 
   public User findOrRegisterUser(FirebaseToken decodedToken, String userName) {
     var uid = decodedToken.getUid();
